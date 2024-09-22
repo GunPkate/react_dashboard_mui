f02dc6912fc04729aeba1858fccd734eb03ca868
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-09-22 15:49:41 +0700

    router and dashboard

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+import { Prosider, Menu, MenuItem } from "react-pro-sidebar"
+import { Box, IconButton, Typography, useTheme } from "@mui/material"
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotAccessibleOutlinedIcon from "@mui/icons-material/NotAccessibleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+
 const Sidebar = () => {
     return <div>
         Sidebar
