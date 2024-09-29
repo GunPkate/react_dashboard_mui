8bf8f53cd8727d05e1402b09e0dfdbf039936c9f
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-09-29 15:21:28 +0700

    start team page

---
+++ src/scenes/team/index.jsx
@@ -0,0 +1,16 @@
+import { Box, Typography, useTheme } from "@mui/material"
+import { DataGrid } from "@mui/x-data-grid"
+import { tokens } from "../../theme"
+import { mockDataTeam } from "../../data/data"
+import { AdminPanelSettingsOutlinedIcon } from "@mui/icons-material/AdminPanelSettingsOutlined"
+import { LockOpenOutlinedIcon } from "@mui/icons-materia/LockOpenOutlined"
+import { SecurityOutlinedIcon } from "@mui/icons-material/SecurityOutlined"
+import Header from "../../components/Header"
+
+export default function Team() {
+    return (
+        <>
+
+        </>
+    )
+}
