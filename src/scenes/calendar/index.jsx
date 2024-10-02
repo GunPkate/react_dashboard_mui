3b2f766122dbaf51f7361c3a6e95a5c8dd21cfac
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-02 21:41:53 +0700

    calendar set foundation

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,10 +1,10 @@
-import fullcalendar, { formatDate } from "@fullcalendar/react";
 import { useState } from "react";
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import listGridPlugin from '@fullcalendar/list'
+import FullCalendar, { formatDate } from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 
-// import interactionPlugin from '@fullcalendar/interaction'
 import { Box, Typography, List, ListItem, ListItemText, useTheme } from "@mui/material";
 import Header from "../../components/Header";
 import { tokens } from "../../theme";
@@ -14,8 +14,39 @@ export default function Calendar() {
     const colors = tokens(theme.palette.mode)
     const [event, setEvent] = useState([]);
 
-    
-    return (<Box>
-        <Header title="Calendar" Subtitle="Calendar"/>
-    </Box>)
+    const handleClickDate = (selected) => {
+        const title = prompt("Please enter new title for event")
+        const calendarApi = selected.view.Calendar
+        calendarApi.unselect()
+
+
+        if (title) {
+            calendarApi.addEvent({
+                id: `${selected.dateStr} - ${title}`,
+                title,
+                start: selected.startStr,
+                end: selected.endStr,
+                allDay: selected.allDay,
+            })
+        }
+    }
+
+    const addEventClick = (selected) => {
+        if (window.confirm(`Are you sure to delete event ${selected.event.title}`)) {
+            selected.event.remove()
+        }
+    }
+
+    return (<Box m="20px">
+        <Header title="Calendar" Subtitle="Calendar" />
+
+        <Box display="flex" justifyContent="space-between">
+
+            <Box flex="1 20%" backgroundColor={colors.primary[400]}  p="15px" borderRadius="4px" >
+                <Typography variant="5h">Event</Typography>
+                <List>
+                </List>
+            </Box>
+    </Box> 
+    </Box >)
 }
