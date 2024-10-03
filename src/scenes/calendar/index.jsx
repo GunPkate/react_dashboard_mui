2da0fbcb791cbab39d1b8b174ebe7f5207f733ae
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-03 16:31:49 +0700

    change version and add calendar

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -12,11 +12,11 @@ import { tokens } from "../../theme";
 export default function Calendar() {
     const theme = useTheme()
     const colors = tokens(theme.palette.mode)
-    const [event, setEvent] = useState([]);
+    const [currentEvents, setCurrentEvents] = useState([]);
 
-    const handleClickDate = (selected) => {
+    const handleDateClick = (selected) => {
         const title = prompt("Please enter new title for event")
-        const calendarApi = selected.view.Calendar
+        const calendarApi = selected.view.calendar
         calendarApi.unselect()
 
 
@@ -31,7 +31,7 @@ export default function Calendar() {
         }
     }
 
-    const addEventClick = (selected) => {
+    const handleEventClick = (selected) => {
         if (window.confirm(`Are you sure to delete event ${selected.event.title}`)) {
             selected.event.remove()
         }
@@ -45,8 +45,53 @@ export default function Calendar() {
             <Box flex="1 20%" backgroundColor={colors.primary[400]}  p="15px" borderRadius="4px" >
                 <Typography variant="5h">Event</Typography>
                 <List>
+                    {currentEvents.map((event) => (
+                        <ListItem
+                            key={event.id}
+                            sx={{ backgroundColor: colors.greenAccent[500], margin: "10px 0", borderRadius: "5px" }}
+                        >
+                            <ListItemText
+                                primary={event.title}
+                                secondary={
+                                    <Typography>
+                                        {formatDate(event.start, {
+                                            year: "numeric",
+                                            month: "short",
+                                            day: "numeric"
+                                        })}
+                                    </Typography>
+                                }
+                            >
+
+                            </ListItemText>
+                        </ListItem>
+                    ))}
                 </List>
             </Box>
-    </Box> 
+
+            <Box flex="1 1 100%" ml="15px">
+                <FullCalendar
+                    height="70vh"
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+                    headerToolbar={{
+                        left: "prev next today",
+                        center: "title",
+                        right: "dayGridMonth timeGridWeek timeGridDay listMonth",
+                    }}
+                    initialView = "dayGridMonth"
+                    editable = {true} 
+                    selectable = {true}
+                    selectMirror = {true}
+                    dayMaxEvents = {true}
+                    select={handleDateClick}
+                    eventClick={handleEventClick}
+                    eventsSet={(events)=> setCurrentEvents(events)}
+                    initialEvents={[
+                        {id: "123", title:"All Day Event", date:"2024-09-14"},
+                        {id: "123", title:"All Day Event", date:"2024-10-02"},
+                    ]}
+                />
+            </Box>
+        </Box>
     </Box >)
 }
