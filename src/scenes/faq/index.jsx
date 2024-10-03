efe132e52b434d0b2b2351f80062b5777552d4eb
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-03 19:30:42 +0700

    Faq list

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@emotion/react"
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
-import { Accordion, AccordionSummary, Box, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
 
@@ -10,9 +10,15 @@ export default function Faq(){
     const colors = tokens(theme.palette.mode)
     return (
         <Box m="10px">
-            <Header Subtitle="FAQ"></Header>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography color={colors.greenAccent[500]} variant="h4">Important Question</Typography>
+                </AccordionSummary>
 
-
+                <AccordionDetails>
+                    <Typography color={colors.grey[200]} variant="h5">Important Question</Typography>
+                </AccordionDetails>
+            </Accordion>
         </Box>
 
     )
